7d7bcc282723f6a9a1c1bcc3bccfe95416fb4848
Michael Lesniak <mlesniak@micromata.de>
2018-06-19 05:33:49 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,41 @@
+let size = 40;
+let cols;
+let rows;
+
+let grid = undefined;
+
+function setup() {
+    createCanvas(400, 400);
+    cols = width / size;
+    rows = height / size;
+
+    grid = new Array(cols);
+    for (let x = 0; x < cols; x++) {
+        grid[x] = new Array(rows);
+    }
+
+    for (let x = 0; x < cols; x++) {
+        for (let y = 0; y < cols; y++) {
+            grid[x][y] = floor(random(2));
+        }
+    }
+}
+
+
+function draw() {
+    background(0);
+    for (let x = 0; x < cols; x++) {
+        for (let y = 0; y < rows; y++) {
+            if (grid[x][y] == 1) {
+                stroke(0);
+                fill(0);
+            } else {
+                stroke(255);
+                fill(255);
+            }
+            
+            rect(x * size, y * size, size-1, size-1);
+            
+        }
+    }
+}
